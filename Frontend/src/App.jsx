8b0422ddec1c9200a301fd8e477559ea6621288b
SHUBHAM PAL <140602488+ShubhamPal23@users.noreply.github.com>
2024-10-16 20:47:29 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,25 +3,27 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Signup from "./Signup";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login";
-
+import "./App.css"
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Recommendation from "./components/Recommendation";
 import Genre from "./components/Genre";
+import Trending from "./components/Trending";
 
 
 function App() {
   return (
-    <div>
+    <div id="root">
       
       <BrowserRouter>
       <Navbar />
         <Routes>
-          {/* <Route path="/register" element={<Signup />}></Route>
-          <Route path="/login" element={<Login />}></Route> */}
+          <Route path="/register" element={<Signup />}></Route>
+          <Route path="/login" element={<Login />}></Route>
           <Route path="/" element={<Home/>}></Route>
-          {/* <Route path="/recommendation" element={<Recommendation/>}></Route> */}
-          {/* <Route path="/genre" element={<Genre/>}></Route> */}
+          <Route path="/recommendation" element={<Recommendation/>}></Route>
+          <Route path="/genre" element={<Genre/>}></Route>
+          <Route path="/trending" element={<Trending/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
