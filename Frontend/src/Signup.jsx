a08359e2c5a6da0650acf6c4b088f9f9ef9db530
Lakshay <110293445+Lakshay404@users.noreply.github.com>
2024-10-18 04:09:59 +0530

    Update Signup.jsx
    Check localhost

--- Frontend/src/Signup.jsx
+++ Frontend/src/Signup.jsx
@@ -2,44 +2,45 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import './Signup.css';
 
-function Signup() {
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+export const Signup = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
-      .post("http://localhost:3001/register", { name, email, password })
+      .post("http://localhost:5500/register", { name, email, password })
       .then((result) => {
         console.log(result);
-        navigate('/login')
+        navigate('/login');
       })
       .catch((err) => console.log(err));
   };
 
   return (
-    <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
-      <div className="bg-white p-3 rounded w-25">
-        <h2>Register</h2>
+    <div className="container">
+      <div className="form-card">
+        <h2 className="form-title">Register</h2>
         <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label htmlFor="email">
+          <div className="form-group">
+            <label htmlFor="name" className="form-label">
               <strong>Name</strong>
             </label>
             <input
               type="text"
               placeholder="Enter Name"
               autoComplete="off"
-              name="email"
-              className="form-control rounded-0"
+              name="name"
+              className="form-control"
               onChange={(e) => setName(e.target.value)}
             />
           </div>
-          <div className="mb-3">
-            <label htmlFor="email">
+          <div className="form-group">
+            <label htmlFor="email" className="form-label">
               <strong>Email</strong>
             </label>
             <input
@@ -47,12 +48,12 @@ function Signup() {
               placeholder="Enter Email"
               autoComplete="off"
               name="email"
-              className="form-control rounded-0"
+              className="form-control"
               onChange={(e) => setEmail(e.target.value)}
             />
           </div>
-          <div className="mb-3">
-            <label htmlFor="email">
+          <div className="form-group">
+            <label htmlFor="password" className="form-label">
               <strong>Password</strong>
             </label>
             <input
@@ -60,24 +61,21 @@ function Signup() {
               placeholder="Enter Password"
               autoComplete="off"
               name="password"
-              className="form-control rounded-0"
+              className="form-control"
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <button type="submit" className="btn btn-success w-100 rounded-0">
+          <button type="submit" className="btn">
             Register
           </button>
         </form>
         <p>Already Have an Account?</p>
-        <Link
-          to="/login"
-          className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none"
-        >
+        <Link to="/login" className="btn btn-login w-100 rounded-0 text-decoration-none">
           Login
         </Link>
       </div>
     </div>
   );
-}
+};
 
 export default Signup;
